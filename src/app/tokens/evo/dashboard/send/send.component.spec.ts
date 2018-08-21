4868c5b9c436e50c49cadfb859b836beb26ea134
Adrian Thompson (Doors) <emperor_jm@yahoo.com>
2018-08-21 16:09:57 -0500

    Base test scripts

--- src/app/tokens/evo/dashboard/send/send.component.spec.ts
+++ src/app/tokens/evo/dashboard/send/send.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SendComponent } from './send.component';
+import { EvoSendComponent } from './send.component';
 
-describe('SendComponent', () => {
-  
+describe('EvoSendComponent', () => {
+
 });
